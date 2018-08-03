bdaf7b761ab319f6416bb54bd4c68b6cf620827c
Jan Piotrowski <piotrowski+github@gmail.com>
2018-08-03 15:23:35 +0200

    rename method + adap UI.messages inline constant that is only used once

--- lib/fastlane/plugin/ionic_integration/helper/ionic_integration_helper.rb
+++ lib/fastlane/plugin/ionic_integration/helper/ionic_integration_helper.rb
@@ -5,26 +5,22 @@ module Fastlane
     class IonicIntegrationHelper
       HELPER_PATH = File.expand_path(File.dirname(__FILE__))
 
-      IOS_RESOURCES_PATH = File.expand_path("#{HELPER_PATH}/../resources/ios")
-
-      #
-      # Copy over to the xcode project our pre-configured UI Test Code
-      #
-      def self.copy_ios_ui_test_code(src_folder, project_folder)
+      def self.copy_all_files(src_folder, project_folder)
         if src_folder && Dir.exist?(src_folder)
           dest_folder = project_folder.to_s
-          UI.message "Copying iOS UI Tests from #{src_folder} to #{dest_folder}"
+          UI.message "Copying all files from #{src_folder} to #{dest_folder}"
           Dir.exist?(dest_folder) || FileUtils.mkdir_p(dest_folder)
           FileUtils.cp_r(src_folder + "/.", dest_folder)
         elsif
-          UI.user_error! "Copying iOS UI Test Files: #{src_folder} does not exist."
+          UI.user_error! "Copying files: #{src_folder} does not exist."
         end
       end
 
       def self.copy_ios_sample_tests(scheme_name)
-        source_folder = "#{IOS_RESOURCES_PATH}/#{IonicIntegration::IONIC_DEFAULT_UNIT_TEST_NAME}"
+        ios_resources_path = File.expand_path("#{HELPER_PATH}/../resources/ios")
+        source_folder = "#{ios_resources_path}/#{IonicIntegration::IONIC_DEFAULT_UNIT_TEST_NAME}"
         dest_folder = "#{IonicIntegration::IONIC_IOS_CONFIG_UITESTS_PATH}/#{scheme_name}"
-        copy_ios_ui_test_code(source_folder, dest_folder)
+        copy_all_files(source_folder, dest_folder)
       end
 
       #
